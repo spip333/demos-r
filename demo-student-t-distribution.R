9c2afc1de41ceced4ff78d1232947f7d02bd547b
(U115136) <nicolas.stern@mobi.ch>
2018-12-09 15:37:01 +0100

    added demo student t distribution

--- demo-student-t-distribution.R
+++ demo-student-t-distribution.R
@@ -10,3 +10,7 @@ plot(x, hx, type="l", lty=2, xlab="t-Verteilungen", ylab="Dichte")
 for (i in 1:4){lines(x, dt(x,degf[i]), lwd=2, col=colors[i])}
 legend("topright", inset=.05, title="Verteilungen",
        labels, lwd=2, lty=c(1, 1, 1, 1, 2), col=colors)
+
+# Bestimmen Sie das 2:5%- und das 97:5%-Perzentil der Studentschen t-Verteilung mit Freiheitsgrad 5.
+
+qt(c(0.025, 0.975), df=5)

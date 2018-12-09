982605d9cc52d592d00cf5122b496390addb637d
(U115136) <nicolas.stern@mobi.ch>
2018-12-09 15:33:33 +0100

    added demo student t distribution

--- demo-student-t-distribution.R
+++ demo-student-t-distribution.R
@@ -5,3 +5,8 @@ hx <- dnorm(x)
 degf <- c(1, 3, 8, 30)
 colors <- c("red", "blue", "darkgreen", "gold", "black")
 labels <- c("df=1", "df=3", "df=8", "df=30", "normal")
+
+plot(x, hx, type="l", lty=2, xlab="t-Verteilungen", ylab="Dichte")
+for (i in 1:4){lines(x, dt(x,degf[i]), lwd=2, col=colors[i])}
+legend("topright", inset=.05, title="Verteilungen",
+       labels, lwd=2, lty=c(1, 1, 1, 1, 2), col=colors)
